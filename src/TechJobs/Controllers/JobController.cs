2f844fcb0b0eb1fcc2113742f94f282247ae2c53
rarose67 <robertrosestl67@gmail.com>
2018-10-04 15:28:56 -0500

    Added function to add a job

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -19,8 +20,8 @@ namespace TechJobs.Controllers
         public IActionResult Index(int id)
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
-
-            return View();
+            Job job = jobData.Find(id);
+            return View(job);
         }
 
         public IActionResult New()
@@ -36,7 +37,23 @@ namespace TechJobs.Controllers
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
 
-            return View(newJobViewModel);
+            if (!(ModelState.IsValid))
+            {
+                return View(newJobViewModel);
+            }
+
+            Job newJob = new Job
+            {
+                Name = newJobViewModel.Name,
+                Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+                PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID)
+
+            };
+
+            jobData.Jobs.Add(newJob);
+            return Redirect("/Job?id=" + newJob.ID);
         }
     }
 }
